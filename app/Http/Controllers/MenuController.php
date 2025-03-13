2522c45fde7961c062f3a9a51e22755da6bb2f1e
Zerghorda <tar.gergo21@gmail.com>
2025-03-13 16:02:58 +0100

    fölösleges redirect rész kitörölve a menu controllelből

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -11,31 +11,26 @@ class MenuController extends Controller
     {
         $menus = response()->json(Menu::all());
         return $menus;
-        redirect()->back();
     }
     public function show($id)
     {
         $menus = response()->json(Menu::find($id));
         return $menus;
-        redirect()->back();
     }
     public function destroy($id)
     {
         Menu::find($id)->delete();
-        redirect()->back();
     }
     public function store(Request $request)
     {
         $menus = new Menu();
         $menus->fill($request->all());
         $menus->save();
-        redirect()->back();
     }
     public function update(Request $request, $id)
     {
         $menus = Menu::find($id);
         $menus->fill($request->all());
         $menus->save();
-        redirect()->back();
     }
 }
